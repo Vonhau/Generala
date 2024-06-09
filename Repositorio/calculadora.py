5c10326d06e3e54becca97347185fc56baec18d8
Joaquin Teixido <joateixido15@gmail.com>
2024-06-09 16:36:21 -0300

    Agregue la generala y la calculadora

---
+++ Repositorio/calculadora.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+# Crear la ventana principal
+ventana = tk.Tk()
+ventana.title("Calculadora (PROYECTO)")
+ventana.config(bg='#434750')
+ventana.geometry("500x300")
+ventana.mainloop()
